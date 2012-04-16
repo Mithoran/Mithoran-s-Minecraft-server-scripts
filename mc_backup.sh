914da746dfa036a48f42c0ffefab8990044679b0
Charles Tucker <ctucker2@uiuc.edu>
2012-04-16 13:37:47 -0500

    Made mc_backup more tolerant of save-all not being instant

--- mc_backup.sh
+++ mc_backup.sh
@@ -3,6 +3,7 @@
 cd /home/cetucker/Documents/minecraft/
 screen -S minecraft -p 0 -X stuff "save-off$(printf \\r)"
 screen -S minecraft -p 0 -X stuff "save-all$(printf \\r)"
+sleep 10s
 git add world
 git ci -m "`date`"
 screen -S minecraft -p 0 -X stuff "save-on$(printf \\r)"
